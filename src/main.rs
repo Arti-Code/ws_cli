d9bf7dbea504cbdc7f2d253365c77b1cdc993fec
Arturo <41492101+Arti-Code@users.noreply.github.com>
2026-06-01 00:56:00 +0200

    v0.7.1

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,8 @@ async fn main() {
     let user_name: &'static str  = Box::leak(user_name.into_boxed_str());
     let url = get_connection_address().await;
     let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-    //let tx = Arc::new(tokio::sync::Mutex::new(stdin_tx));
-    //let tx1 = tx.clone();
     tokio::spawn(send_message(stdin_tx.clone(), &user_name));
-    print!("connecting to {}...", &url);
     let ws_stream = establish_connection(&url).await;
-    println!("[ok]");
     send_command(stdin_tx.clone(), &Command::RegisterUserName(user_name.to_string())).await;
     let (write, read) = ws_stream.split();
     let stdin_to_ws = stdin_rx.map(Ok).forward(write);
@@ -51,14 +47,13 @@ async fn main() {
 }
 
 async fn establish_connection(url: &str) -> WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>> {
-    //print!("connecting to: {}...", url);
-    //let msg = format!("connecting to: {}...", url);
-    //std::io::stdout().write_all(msg.as_bytes()).expect("ups!");
-    _ = std::io::stdout().flush().expect("ups!");
+    print!("connecting to: {}...", url);
     tokio::time::sleep(Duration::from_secs(1)).await;
-    let (ws_stream, _) = connect_async(url).await.expect("failed to connect");
-    //println!("[OK]");
-    //println!("---------------------");
+    let (ws_stream, _) = connect_async(url)
+    .await.expect("failed to connect");
+    println!("{}", "[OK]".green());
+    println!("---------------------");
+    _ = std::io::stdout().flush().expect("ups!");
     ws_stream
 }
 
@@ -67,22 +62,14 @@ async fn recv_messages(msg: Message) {
         .expect("unreadable data");
     let mut text = text.to_string();
     text = text.trim().to_string();
-    //_ = std::io::stdout().write_all(text.as_bytes());
-    println!("{}", text.bold());
-    //text.insert_str(0, "[↘︎]");
-    //tokio::io::stdout().write_all(text.into_bytes().as_slice())
-    //    .await.expect("can't write a message");
+    println!("{}", text.bright_blue());
 }
 
 async fn send_message(tx: ChatSender, user_name: &str) {
-    //let mut stdin0 = tokio::io::stdin();
     let user = format!("[{}] ", user_name).as_bytes().to_vec();
     loop {
-        //print!("[Me]: ");
-        //_ = std::io::stdout().flush();
         let mut stdin = tokio::io::stdin();
         let mut buf = vec![0; 1024];
-        //stdin.chain(next)
         let n = match stdin.read(&mut buf).await {
             Err(_) | Ok(0) => break,
             Ok(n) => n,
@@ -98,8 +85,6 @@ async fn send_message(tx: ChatSender, user_name: &str) {
             None => {
                 let mut msg = user.clone(); 
                 msg.extend_from_slice(&buf);
-                //let my_msg = String::from_utf8_lossy(&msg).to_string();
-                //println!("{}", my_msg.green());
                 unsafe {
                     let text = Utf8Bytes::from_bytes_unchecked(msg.into());
                     if tx.unbounded_send(Message::Text(text)).is_err() {
@@ -116,10 +101,6 @@ async fn check_command(input: &str) -> Option<Command> {
     match trimmed {
         "/quit" => Some(Command::Quit),
         "/list" => Some(Command::ListUsers),
-        //cmd if cmd.starts_with("/register ") => {
-        //    let name = cmd.trim_start_matches("/register ").to_string();
-        //    Some(Command::RegisterUserName(name))
-        //}
         _ => None,
     }
 }
@@ -129,7 +110,6 @@ async fn send_command(mut tx: ChatSender, command: &Command) -> bool {
     let cmd_msg = match command {
         Command::Quit => {
             tx.disconnect();
-            //tx.close_channel();
             let msg = "connection closed".to_string().red();
             println!("{}", msg);
             closing = true;
@@ -142,7 +122,6 @@ async fn send_command(mut tx: ChatSender, command: &Command) -> bool {
             format!("/register {}", name)
         }
     };
-    //let text = Utf8Bytes::from_bytes_unchecked(cmd.into());
     let _ = tx.unbounded_send(Message::Text(cmd_msg.into()));
     return closing;
 }
@@ -153,7 +132,8 @@ async fn register_name() -> String {
     let mut buf = String::new();
     _ = std::io::stdin().read_line(&mut buf).expect("failed to register user name");
     let name = buf.trim();
-    println!("Welcome, {}!", name.green());
+    let m = format!("Welcome, {}!", name).green().bold();
+    println!("{}", m);
     name.to_string()
 }
 
@@ -161,9 +141,10 @@ async fn init_display() {
     let ver = env!("CARGO_PKG_VERSION");
     let author = env!("CARGO_PKG_AUTHORS");
     println!(" ");
-    let title = "-=WebSocket Chat Client=-".to_string().cyan();
+    let title = "WebSocket Chat".to_string().bright_blue().bold().underline();
     println!("{}", title);
-    println!("{}: {} |  {}", "ver", ver, author);
+    let version_info = format!("ver.{} |  {}", ver, author).italic().bright_yellow();
+    println!("{}", version_info);
     println!(" ");
     tokio::time::sleep(Duration::from_secs(1)).await;
 }
@@ -171,7 +152,8 @@ async fn init_display() {
 async fn get_connection_address() -> String {
     match env::args().nth(1) {
         Some(url) => url,
-        None => "ws://127.0.0.1:8080".to_string(),
-        //None => "ws://yamanote.proxy.rlwy.net:26134".to_string(),
+        //None => "ws://127.0.0.1:8080".to_string(),
+        None => "ws://zephyr.proxy.rlwy.net:19310".to_string(),
     }
 }
+
